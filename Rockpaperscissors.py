bfa24088d73a325336eb8e14c213af3da104a41d
vijay-mundkar <149820691+vijay-mundkar@users.noreply.github.com>
2023-11-13 21:01:44 +0530

    Add files via upload

---
+++ Rockpaperscissors.py
@@ -0,0 +1,18 @@
+import random
+user_choice = int(input("Enter your choice: Type 0 for Rock ,1 for paper ,2 for Scissors : "))
+if user_choice >= 3 or user_choice < 0:
+    print("You Entered invalid number,please enter valid number :")
+else:
+    computer_choice = random.randint(0,2)  
+    print("Computer choice :")
+    print(computer_choice)
+    if computer_choice == user_choice:
+        print("It is a Draw")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You lose")
+    elif user_choice == 0 and computer_choice == 2:
+        print("you win")
+    elif computer_choice > user_choice:
+        print("You lose")
+    elif user_choice > computer_choice:
+        print("You Win")
